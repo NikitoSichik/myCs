52c051bd08381ef2cb5ff73ff94c7cdb07ec97aa
Nikita_Smerti_SS+_Class <lpupti@gmail.com>
2026-03-23 14:55:54 +0300

    testova

--- myCSharp/myCSharp/Program5.cs
+++ myCSharp/myCSharp/Program5.cs
@@ -1,23 +1,23 @@
-﻿struct Point2D
-{
-    public Point2D(int  x, int y)
-    {
-        this.x = x;
-        this.y = y;
-    }
-    public Point2D() : this(10, 10) { }
-    public int x { get; set; }
-    public int y { get; set; }
-}
+﻿//struct Point2D
+//{
+//    public Point2D(int  x, int y)
+//    {
+//        this.x = x;
+//        this.y = y;
+//    }
+//    public Point2D() : this(10, 10) { }
+//    public int x { get; set; }
+//    public int y { get; set; }
+//}
 
-class Program
-{
-    static void Main()
-    {
-        Point2D a = new Point2D();
-        Point2D b = a;
-        Console.WriteLine($"b.x: {b.x} b.y: {b.y} | a.x: {a.x} a.y: {a.y}");
-        a.x = 122; a.y = 512;
-        Console.WriteLine($"b.x: {b.x} b.y: {b.y} | a.x: {a.x} a.y: {a.y}");
-    }
-}
+//class Program
+//{
+//    static void Main()
+//    {
+//        Point2D a = new Point2D();
+//        Point2D b = a;
+//        Console.WriteLine($"b.x: {b.x} b.y: {b.y} | a.x: {a.x} a.y: {a.y}");
+//        a.x = 122; a.y = 512;
+//        Console.WriteLine($"b.x: {b.x} b.y: {b.y} | a.x: {a.x} a.y: {a.y}");
+//    }
+//}
--- myCSharp/myCSharp/Program6.cs
+++ myCSharp/myCSharp/Program6.cs
@@ -1,1 +1,32 @@
-﻿
+﻿//class Program
+//{
+//    static void ModifyItem(ref int item)
+//    {
+//        item += 5;
+//    }
+
+//    static void ModifyItems(List<string> items) {
+
+//        for (int i = 0; i < items.Count; i++)
+//        {
+//            items[i] = $"{i + 1}. {items[i]}";
+//        }
+//    }
+
+//    static void Main()
+//    {
+//        var number = 0;
+//        var items = new List<string>() { "Hello", "World" };
+
+//        Console.WriteLine(number);
+//        foreach (var item in items) Console.WriteLine(item);
+
+//        Console.WriteLine("\nПосле изменений\n");
+
+//        ModifyItem(ref number);
+//        ModifyItems(items);
+
+//        Console.WriteLine(number);
+//        foreach (var item in items) Console.WriteLine(item);
+//    }
+//}
